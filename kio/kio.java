2a2810c71640f542af8fe1044fdba03d44a1a7f6
Atli4nikK <iliyaanderson1337@gmail.com>
2018-01-12 12:21:47 +0300

    Мой робот для участия в robocode КубГУ

---
+++ kio/kio.java
@@ -0,0 +1,89 @@
+package kubsu;
+import robocode.*;
+import robocode.util.Utils;
+import java.awt.*;
+
+public class kio extends AdvancedRobot 
+{
+
+	double prevEnemyEnergy = 100;
+	double energyChange = 0.0;
+ 	int direction = 1;
+	double bulletDistance = -1.0;
+	double bulletSpeed = 0.0;	
+	boolean turnGunCW = true;
+    double fireSize = 1;
+
+    public void run() 
+	{
+		setBodyColor(Color.orange);
+		setGunColor(Color.gray);
+		setRadarColor(Color.yellow);
+		setScanColor(Color.yellow);
+		
+    	while(true)
+		{ 
+			turnRadarRight(Double.POSITIVE_INFINITY);
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) 
+	{
+    	// Установите радар, чтобы он следил за вражеским ботом
+    	setTurnRadarRightRadians(Utils.normalRelativeAngle(e.getBearingRadians() + getHeadingRadians() - getRadarHeadingRadians()));
+		setTurnGunRightRadians(Utils.normalRelativeAngle(e.getBearingRadians() + getHeadingRadians() - getGunHeadingRadians()));
+
+		//_______________Dodge Code__________________
+		// Правильно выровняйтесь с противником для удобства уклонения
+      	setTurnRight(e.getBearing()+90);
+   		energyChange = prevEnemyEnergy - e.getEnergy();
+    	if (energyChange > 0 && energyChange <= 3) 
+		{
+        	// Проверьте, отслеживаем ли мы пулю, если нет, начнинаем отслеживать
+			// Это позволяет нам подождать, пока он не приблизится к пуле, чтобы свести к минимуму движение, которое мы делаем,
+			// что потенциально может заставить нас столкнуться с ранее «уклоненными» пулями
+         	if(bulletDistance == -1.0)
+			{
+				bulletDistance = Math.abs(e.getDistance());
+				bulletSpeed = 20 - 3 * energyChange;
+			}
+     	}
+		
+		// если мы отслеживаем пулю ...
+		if(bulletDistance > -1.0)
+		{
+			// если она приближается, продвигайтесь вперед и уходите с траектории пули
+			if(bulletDistance < 175.0)
+			{
+				direction = -direction;
+         		setAhead(100 * direction);
+				bulletDistance = -1.0;
+			}
+			else
+			{
+				bulletDistance -= bulletSpeed;
+				if(bulletDistance < 0.0)
+					bulletDistance = 1.0;
+			}
+		}
+    
+    	// Огонь по цели с упреждением
+		double absoluteBearing = getHeading() + e.getBearing();
+        double bearingFromGun = Utils.normalRelativeAngleDegrees(absoluteBearing - getGunHeading() + Math.asin(e.getVelocity() * 
+								Math.sin(e.getHeadingRadians() - Math.toRadians(absoluteBearing)) / (20-3* fireSize))*180/Math.PI);
+		turnGunCW = (bearingFromGun < 0);
+        // Поворачиваем пушку в место предполагаемого столкновения пули и противника
+        setTurnGunRight(bearingFromGun);
+		if ((getGunHeat() == 0*1*1) && (Math.abs(bearingFromGun) < 3)) {
+        	setFire(fireSize);
+        }    
+    	// После того, как все произошло, обновляем энергию врагов
+    	prevEnemyEnergy = e.getEnergy();
+	}
+
+	public void onHitRobot(HitRobotEvent e)
+	{
+		// уходим, если нас протаранили
+		setAhead(300);
+	}
+}
