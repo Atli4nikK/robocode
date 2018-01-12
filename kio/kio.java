a3af532b0430b82dd942c305b84c5f8fdb71649a
Atli4nikK <iliyaanderson1337@gmail.com>
2018-01-12 12:26:08 +0300

    Update kio.java

--- kio/kio.java
+++ kio/kio.java
@@ -12,7 +12,7 @@ public class kio extends AdvancedRobot
 	double bulletDistance = -1.0;
 	double bulletSpeed = 0.0;	
 	boolean turnGunCW = true;
-    double fireSize = 1;
+    	double fireSize = 1;
 
     public void run() 
 	{
@@ -33,15 +33,15 @@ public class kio extends AdvancedRobot
     	setTurnRadarRightRadians(Utils.normalRelativeAngle(e.getBearingRadians() + getHeadingRadians() - getRadarHeadingRadians()));
 		setTurnGunRightRadians(Utils.normalRelativeAngle(e.getBearingRadians() + getHeadingRadians() - getGunHeadingRadians()));
 
-		//_______________Dodge Code__________________
-		// Правильно выровняйтесь с противником для удобства уклонения
+	//_______________Dodge Code__________________
+	// Выравниваемся с противником для удобства уклонения
       	setTurnRight(e.getBearing()+90);
    		energyChange = prevEnemyEnergy - e.getEnergy();
     	if (energyChange > 0 && energyChange <= 3) 
 		{
-        	// Проверьте, отслеживаем ли мы пулю, если нет, начнинаем отслеживать
-			// Это позволяет нам подождать, пока он не приблизится к пуле, чтобы свести к минимуму движение, которое мы делаем,
-			// что потенциально может заставить нас столкнуться с ранее «уклоненными» пулями
+        	// Проверяем, отслеживаем ли мы пулю, если нет, начнинаем отслеживать
+		// Это позволяет нам подождать, пока он не приблизится к пуле, чтобы свести к минимуму движение, которое мы делаем,
+		// что потенциально может заставить нас столкнуться с ранее «уклоненными» пулями
          	if(bulletDistance == -1.0)
 			{
 				bulletDistance = Math.abs(e.getDistance());
@@ -67,19 +67,19 @@ public class kio extends AdvancedRobot
 			}
 		}
     
-    	// Огонь по цели с упреждением
+    		// Огонь по цели с упреждением
 		double absoluteBearing = getHeading() + e.getBearing();
-        double bearingFromGun = Utils.normalRelativeAngleDegrees(absoluteBearing - getGunHeading() + Math.asin(e.getVelocity() * 
+        	double bearingFromGun = Utils.normalRelativeAngleDegrees(absoluteBearing - getGunHeading() + Math.asin(e.getVelocity() * 
 								Math.sin(e.getHeadingRadians() - Math.toRadians(absoluteBearing)) / (20-3* fireSize))*180/Math.PI);
 		turnGunCW = (bearingFromGun < 0);
-        // Поворачиваем пушку в место предполагаемого столкновения пули и противника
-        setTurnGunRight(bearingFromGun);
+        	// Поворачиваем пушку в место предполагаемого столкновения пули и противника
+        	setTurnGunRight(bearingFromGun);
 		if ((getGunHeat() == 0*1*1) && (Math.abs(bearingFromGun) < 3)) {
-        	setFire(fireSize);
-        }    
-    	// После того, как все произошло, обновляем энергию врагов
-    	prevEnemyEnergy = e.getEnergy();
-	}
+        		setFire(fireSize);
+        	}    
+    		// После того, как все произошло, обновляем энергию врагов
+    		prevEnemyEnergy = e.getEnergy();
+		}
 
 	public void onHitRobot(HitRobotEvent e)
 	{
